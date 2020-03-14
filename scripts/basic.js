76a854b12d5463c689c4791d02a2e385ff17025b
David Sergeev <d_sergeevi@cu.edu.ge>
2020-03-14 19:19:48 +0400

    optimization

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,10 +1,11 @@
 'use strict';
 function get_ajax(datas,ajax_file_path,div_id,method,html = 1,succesfunction = 0) {
 	$.ajaxSetup({cache: false});
+	let root = $("#documentRoot").val();
 	
 	$.ajax({
 		'type': method,
-		'url': ajax_file_path,
+		'url': root + ajax_file_path,
 		'data': {'datas': datas},
 		'success': function(data) {
 			if(!succesfunction) {
@@ -32,11 +33,11 @@ setInterval(function () {
 }, 1000);
 
 setInterval(function () {
-	get_ajax({'CURRENT_SPORTSMEN' : $('#current_sportsmen').html(), 'JUDGE_ID' : $('#judge_id').val()},'/Acrochamp/includes/basic/pause.php','#pause','POST', 1);
+	get_ajax({'CURRENT_SPORTSMEN' : $('#current_sportsmen').html(), 'JUDGE_ID' : $('#judge_id').val()},'/includes/basic/pause.php','#pause','POST', 1);
 }, 3000);
 
 $(document).ready(function() {
-	get_ajax({'CURRENT_SPORTSMEN' : $('#current_sportsmen').html(), 'JUDGE_ID' : $('#judge_id').val()},'/Acrochamp/includes/basic/pause.php','#pause','POST', 1);
+	get_ajax({'CURRENT_SPORTSMEN' : $('#current_sportsmen').html(), 'JUDGE_ID' : $('#judge_id').val()},'/includes/basic/pause.php','#pause','POST', 1);
 	
 //	window.onbeforeunload = function() {
 //        return false;
